2996dd269310bdc3412505bd36c92d72c03564d2
Mindsers <dev@nathanaelcherrier.com>
2017-12-16 22:59:55 +0100

    Get form data before submiting

--- js/main.js
+++ js/main.js
@@ -13,5 +13,21 @@
 })()
 
 function application() {
+  document
+    .querySelector('#contact-form')
+    .addEventListener('submit', handleContactFormSubmit)
+}
 
+function handleContactFormSubmit(e) {
+  e.preventDefault()
+
+  const form = e.target
+  const url = form.getAttribute('action')
+
+  const name = form.querySelector('input[name="mail-nom"]').value
+  const addr = form.querySelector('input[name="mail-addr"]').value
+  const obj = form.querySelector('input[name="mail-objet"]').value
+  const msg = form.querySelector('textarea[name="mail-message"]').value
+
+  console.table({ url, name, addr, obj, msg })
 }
