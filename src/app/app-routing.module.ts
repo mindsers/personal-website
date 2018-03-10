f953988e6451248b91b0db1ee578e0e72994c465
Mindsers <dev@nathanaelcherrier.com>
2018-03-11 00:47:20 +0100

    Remove header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
-import { RouterModule } from '@angular/router'
+import { RouterModule, Routes } from '@angular/router'
 
-const routes = []
+const routes: Routes = []
 
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,18 @@ import { ServiceWorkerModule } from '@angular/service-worker'
 import { AppComponent } from './app.component'
 
 import { environment } from '../environments/environment'
-import { HeaderComponent } from './header/header.component'
 import { SharedModule } from './shared/shared.module'
 import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     SharedModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
