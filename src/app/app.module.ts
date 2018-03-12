8fa6297d3e23f1ac9c3aed9c40c100a60486b0b6
Mindsers <dev@nathanaelcherrier.com>
2018-03-12 23:40:55 +0100

    Create home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { environment } from '../environments/environment'
 import { SharedModule } from './shared/shared.module'
 import { AppRoutingModule } from './app-routing.module'
 import { ResumeComponent } from './resume/resume.component'
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResumeComponent
+    ResumeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
