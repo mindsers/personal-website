52b436b52110cdabeced72e3dddd294178c8e9bd
Mindsers <dev@nathanaelcherrier.com>
2018-03-18 16:37:56 +0100

    Add validation on the contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 import { NgForm } from '@angular/forms'
 
 @Component({
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
-  ngOnInit() {}
-
+export class ContactComponent {
   handleSubmit(form: NgForm) {}
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common'
 import { InstagramService } from './instagram.service'
 import { PopinComponent } from './popin/popin.component'
 import { StopClickPropagationDirective } from './stop-click-propagation.directive'
+import { MatchValidatorDirective } from './match-validator.directive'
+import { EmailValidatorDirective } from './email-validator.directive'
 
 @NgModule({
-  declarations: [PopinComponent, StopClickPropagationDirective],
+  declarations: [PopinComponent, StopClickPropagationDirective, MatchValidatorDirective, EmailValidatorDirective],
   imports: [CommonModule],
-  exports: [PopinComponent, StopClickPropagationDirective],
+  exports: [PopinComponent, StopClickPropagationDirective, MatchValidatorDirective, EmailValidatorDirective],
   providers: [InstagramService]
 })
 export class SharedModule { }
