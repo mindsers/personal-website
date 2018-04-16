8f98e79ce4d2a09f13be429a064ae00571eac268
Mindsers <dev@nathanaelcherrier.com>
2018-04-16 09:37:14 +0200

    Open information popin on resume loading error

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 
 import { ResumeService } from './resume.service'
+import { PopinService } from '../shared/popin/popin.service'
 
 @Component({
   templateUrl: './resume.component.html',
@@ -13,7 +14,7 @@ export class ResumeComponent implements OnInit {
 
   private experiences = []
 
-  constructor(private resumeService: ResumeService) {}
+  constructor(private resumeService: ResumeService, private popinService: PopinService) {}
 
   ngOnInit() {
     this.resumeService.getResume()
@@ -44,9 +45,17 @@ export class ResumeComponent implements OnInit {
 
   openResumeFile() {
     this.resumeService.getResumeFile()
-      .forEach(file => {
-        window.open(file)
-      })
+      .subscribe(
+        file => window.open(file),
+        error => {
+          this.popinService
+            .openPopin(null, {
+              message: 'An error occured. We are unable to open the resume.'
+            })
+            .afterClose()
+            .subscribe()
+        }
+      )
   }
 }
 
