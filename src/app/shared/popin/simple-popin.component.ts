50b7b2e3284810d8afcc4566480632bee2280561
Mindsers <dev@nathanaelcherrier.com>
2018-04-16 09:36:41 +0200

    Get data by injection in simplepopin

--- src/app/shared/popin/simple-popin.component.ts
+++ src/app/shared/popin/simple-popin.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core'
+import { Component, Inject, OnInit } from '@angular/core'
+
+import { POPIN_VIEWREF, POPIN_DATA } from './popin'
 
 @Component({
   template: `
@@ -11,13 +13,33 @@ import { Component } from '@angular/core'
   `,
   styleUrls: ['./simple-popin.component.scss']
 })
-export class SimplePopinComponent {
-  message = 'This is the default popin. You might have forgotten to customize your popin.'
-  actions = [
-    { label: 'Valider', value: null }
-  ]
+export class SimplePopinComponent implements OnInit {
+  message: string
+  actions: { label: string, value: any }[]
+
+  constructor(
+    @Inject(POPIN_VIEWREF) private popinRef,
+    @Inject(POPIN_DATA) private data: SimplePopinData = {}
+  ) {}
+
+  ngOnInit() {
+    const {
+      message = 'This is the default popin. You might have forgotten to customize your popin.',
+      actions = [
+        { label: 'OK', value: null }
+      ]
+    } = this.data
+
+    this.actions = actions
+    this.message = message
+  }
 
   handleClick(value) {
-    // close
+    this.popinRef.close(value)
   }
+}
+
+interface SimplePopinData {
+  message?: string
+  actions?: { label: string, value: any }[]
 }
