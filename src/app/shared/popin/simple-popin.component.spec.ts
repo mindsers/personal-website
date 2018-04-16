dcfac2d997e93e2f1e5e5fb4ec08059e0e8841cd
Mindsers <dev@nathanaelcherrier.com>
2018-04-16 09:29:50 +0200

    Fix lint and bug

--- src/app/shared/popin/simple-popin.component.spec.ts
+++ src/app/shared/popin/simple-popin.component.spec.ts
@@ -1,25 +1,30 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { SimplePopinComponent } from './simple-popin.component';
+import { SimplePopinComponent } from './simple-popin.component'
+import { POPIN_DATA, POPIN_VIEWREF } from './popin'
 
 describe('SimplePopinComponent', () => {
-  let component: SimplePopinComponent;
-  let fixture: ComponentFixture<SimplePopinComponent>;
+  let component: SimplePopinComponent
+  let fixture: ComponentFixture<SimplePopinComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SimplePopinComponent ]
+      declarations: [ SimplePopinComponent ],
+      providers: [
+        { provide: POPIN_DATA, useValue: {} },
+        { provide: POPIN_VIEWREF, useValue: {} }
+      ]
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SimplePopinComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(SimplePopinComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
