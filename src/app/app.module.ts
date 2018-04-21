16e533db7b99255cb7b72f2ef7b85ff3422324a2
Mindsers <dev@nathanaelcherrier.com>
2018-04-21 23:12:58 +0200

    Create ContactService to send contact mail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ResumeComponent } from './resume/resume.component'
 import { HomeComponent } from './home/home.component'
 import { ResumeService } from './resume/resume.service'
 import { ContactComponent } from './contact/contact.component'
+import { ContactService } from './contact/contact.service'
 
 @NgModule({
   declarations: [
@@ -31,7 +32,7 @@ import { ContactComponent } from './contact/contact.component'
     FormsModule
   ],
   entryComponents: [ContactComponent],
-  providers: [ResumeService],
+  providers: [ResumeService, ContactService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
