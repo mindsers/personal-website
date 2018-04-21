905caadf52accbc000dd507ec5a1f3c1618654b1
Mindsers <dev@nathanaelcherrier.com>
2018-04-21 23:13:47 +0200

    Send email on form submit

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core'
 import { NgForm } from '@angular/forms'
 
+import { ContactService } from './contact.service'
+
 @Component({
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
-  handleSubmit(form: NgForm) {}
+  flashMessages: { type: string, text: string }[] = []
+
+  constructor(private contactService: ContactService) {}
+
+  handleSubmit(form: NgForm) {
+    console.log(form.value)
+    const { email, message, subject } = form.value
+
+    this.contactService
+      .sendMail({ sender: email, message, subject })
+      .subscribe(
+        () => this.flashMessages = [{ type: 'success', text: 'Your message has been send.' }],
+        () => this.flashMessages.push({ type: 'error', text: 'An error occured when trying to send the message' })
+      )
+  }
 }
