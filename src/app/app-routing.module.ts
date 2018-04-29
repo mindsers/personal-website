a0062d0d0187da86e20167bda2531f403e23898f
Mindsers <dev@nathanaelcherrier.com>
2018-04-29 18:30:38 +0200

    Add route for PageNotFound component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component'
 import { ResumeComponent } from './resume/resume.component'
 import { ContactComponent } from './contact/contact.component'
+import { PageNotFoundComponent } from './errors/page-not-found.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'resume', component: ResumeComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '404' }
 ]
 
 @NgModule({
