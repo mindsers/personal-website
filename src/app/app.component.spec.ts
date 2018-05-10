32ee2f2161368f93510c7024827fc974688a5f35
Mindsers <dev@nathanaelcherrier.com>
2018-05-10 23:36:01 +0200

    Test if popin was opened when handleOnContactButtonClick is called

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed, async } from '@angular/core/testing'
-import { AppComponent } from './app.component'
 import { NO_ERRORS_SCHEMA } from '@angular/core'
+
+import { AppComponent } from './app.component'
 import { PopinService } from './shared/popin/popin.service'
+import { ContactComponent } from './contact/contact.component'
 
 describe('AppComponent', () => {
+  const popinServiceMock = { openPopin: jasmine.createSpy('openPopin') }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -11,13 +15,27 @@ describe('AppComponent', () => {
       ],
       schemas: [NO_ERRORS_SCHEMA],
       providers: [
-        { provide: PopinService, useValue: {} }
+        { provide: PopinService, useValue: popinServiceMock }
       ]
     }).compileComponents()
   }))
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.debugElement.componentInstance
     expect(app).toBeTruthy()
   }))
+
+  describe('#handleOnContactButtonClick()', () => {
+    it('should open contact popin', () => {
+      const fixture = TestBed.createComponent(AppComponent)
+      const app = fixture.debugElement.componentInstance
+
+      popinServiceMock.openPopin.and.returnValue({ afterClose: () => ({ subscribe: () => {} }) })
+
+      app.handleOnContactButtonClick()
+
+      expect(popinServiceMock.openPopin).toHaveBeenCalledWith(ContactComponent)
+    })
+  })
 })
