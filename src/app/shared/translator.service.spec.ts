1bad71d355367d17e8516b739baafd995947a058
Mindsers <dev@nathanaelcherrier.com>
2018-05-23 21:30:55 +0200

    Load basic xml doc to get translation unit

--- src/app/shared/translator.service.spec.ts
+++ src/app/shared/translator.service.spec.ts
@@ -12,4 +12,36 @@ describe('RuntimeTranslatorService', () => {
   it('should be created', inject([RuntimeTranslatorService], (service: RuntimeTranslatorService) => {
     expect(service).toBeTruthy()
   }))
+
+  describe('#load()', () => {
+    it('should load data in default scope when no scope is specified',
+      inject([RuntimeTranslatorService], (service: RuntimeTranslatorService) => {
+        const data = (new DOMParser()).parseFromString(`
+        <xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">
+          <file source-language="en" datatype="plaintext" original="ng2.template">
+            <body>
+              <trans-unit id="home.title" datatype="html">
+                <source>Welcome to this page</source>
+                <target>Welcome to this page</target>
+                <context-group purpose="location">
+                  <context context-type="sourcefile">app/contact/contact.component.ts</context>
+                  <context context-type="linenumber">3</context>
+                </context-group>
+              </trans-unit>
+            </body>
+          </file>
+        </xliff>
+        `, 'text/xml')
+
+        expect(service.load(data))
+          .toEqual({
+            default: {
+              en: [
+                { key: 'home.title', source: 'Welcome to this page', target: 'Welcome to this page' }
+              ]
+            }
+          })
+      })
+    )
+  })
 })
--- src/app/shared/translator.service.ts
+++ src/app/shared/translator.service.ts
@@ -2,5 +2,31 @@ import { Injectable } from '@angular/core'
 
 @Injectable({ providedIn: 'root' })
 export class RuntimeTranslatorService {
+  private data: { [scope: string]: TranslationScope } = {}
+
   constructor() {}
+
+  load(data: XMLDocument, scope: string = 'default') {
+    const units = Array.from(data.getElementsByTagName('trans-unit'))
+
+    this.data[scope] = {
+      en: units.map(element => ({
+        key: element.getAttribute('id'),
+        source: element.getElementsByTagName('source')[0].childNodes[0].nodeValue,
+        target: element.getElementsByTagName('target')[0].childNodes[0].nodeValue
+      }))
+    }
+
+    return this.data
+  }
+}
+
+interface TranslationScope {
+  [language: string]: TranslationString[]
+}
+
+interface TranslationString {
+  key: string
+  source: string
+  target: string
 }
