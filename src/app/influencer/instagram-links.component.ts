fd68232a8fc5debc042a003fb317de29df4203df
Mindsers <dev@nathanaelcherrier.com>
2019-06-17 10:24:28 +0200

    Structure of links page

--- src/app/influencer/instagram-links.component.ts
+++ src/app/influencer/instagram-links.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-instagram-links',
-  templateUrl: './instagram-links.component.html',
+  template: `
+  <div class="instagram-links">
+    <h1>@mindsers.it</h1>
+    <ul class="links">
+      <li><a class="amazon" href="#">My favorite products</a></li>
+      <li><a class="wiki" href="#">How to support MindsersIT</a></li>
+      <li><a class="external" href="#">The best tools to build your blog</a></li>
+      <li><a class="external" href="#">Secure your crypto assets!</a></li>
+      <li><a class="external" href="#">Let's earn some bitcoin together!</a></li>
+    </ul>
+  </div>`,
   styleUrls: ['./instagram-links.component.scss']
 })
-export class InstagramLinksComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class InstagramLinksComponent {}
