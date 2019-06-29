4a4d1fe1566c03ef5bd853673b2da1d319d038e0
Mindsers <dev@nathanaelcherrier.com>
2019-06-29 16:31:16 +0200

    Use the correct links

--- src/app/influencer/instagram-links.component.ts
+++ src/app/influencer/instagram-links.component.ts
@@ -4,11 +4,11 @@ import { Component } from '@angular/core'
   template: `
   <div class="instagram-links">
     <ul class="links">
-      <li><a class="amazon" href="#">My favorite products</a></li>
-      <li><a class="wiki" href="#">How to support MindsersIT?</a></li>
-      <li><a class="external" href="#">The best blogging platform</a></li>
-      <li><a class="external" href="#">Secure your crypto assets!</a></li>
-      <li><a class="external" href="#">Let's earn some bitcoin together!</a></li>
+      <li><a class="amazon" href="https://www.amazon.com/shop/mindsers.it">My favorite products</a></li>
+      <li><a class="wiki" href="https://nathanaelcherrier.com/en/support">How to support MindsersIT?</a></li>
+      <li><a class="external" href="https://ghost.org/?lmref=739">The best blogging platform</a></li>
+      <li><a class="external" href="http://www.ledger.com/?r=bdbc">Secure your crypto assets!</a></li>
+      <li><a class="external" href="https://freebitco.in/?r=9267442">Let's earn some bitcoin together!</a></li>
     </ul>
   </div>`,
   styleUrls: ['./instagram-links.component.scss']
