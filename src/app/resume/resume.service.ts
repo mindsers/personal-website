af8515b5265e52ef99e6970b80d17fed094538ac
Mindsers <dev@nathanaelcherrier.com>
2020-02-17 00:32:25 +0100

    :rotating_light: Remove useless prefix

--- src/app/resume/resume.service.ts
+++ src/app/resume/resume.service.ts
@@ -5,14 +5,12 @@ import { DomSanitizer } from '@angular/platform-browser'
 import { map } from 'rxjs/operators'
 
 import { environment } from '../../environments/environment'
-import { WINDOW } from '../shared/native-api'
 
 @Injectable()
 export class ResumeService {
   constructor(
     private httpService: HttpClient,
-    @Inject(LOCALE_ID) private locale: string,
-    @Inject(WINDOW) private window: Window
+    @Inject(LOCALE_ID) private locale: string
   ) {}
 
   getResume() {
@@ -28,7 +26,7 @@ export class ResumeService {
       .get(`${environment.api}/resume/file?locale=${this.locale}`, { responseType: 'blob' })
       .pipe(
         map(blob => new Blob([blob], { type: 'application/pdf' })),
-        map(blob => this.window.URL.createObjectURL(blob))
+        map(blob => URL.createObjectURL(blob))
       )
   }
 }
