3b4a773bd71ef61fd5733cdab6260d7705fafb8a
Nathanael Cherrier <dev@nathanaelcherrier.com>
2020-08-31 20:55:09 +0200

    Lint + center content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,11 @@ import { UnknownErrorComponent } from './errors/unknown-error.component'
 import { SupportComponent } from './support/support.component'
 import { LegalComponent } from './legal/legal.component'
 import { InstagramLinksComponent } from './influencer/instagram-links.component'
-import { InstagramService } from './influencer/shared/instagram.service';
-import { OpenSourcererComponent } from './open-sourcerer/open-sourcerer.component';
-import { InfluencerComponent } from './influencer/influencer.component';
-import { ContractorComponent } from './contractor/contractor.component';
-import { WriterComponent } from './writer/writer.component';
+import { InstagramService } from './influencer/shared/instagram.service'
+import { OpenSourcererComponent } from './open-sourcerer/open-sourcerer.component'
+import { InfluencerComponent } from './influencer/influencer.component'
+import { ContractorComponent } from './contractor/contractor.component'
+import { WriterComponent } from './writer/writer.component'
 import { TrainerComponent } from './trainer/trainer.component'
 
 @NgModule({
